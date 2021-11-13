3249f3fc507e38f4d8266bcdb32d9eee66b4786b
Noah A Klem <noahaklem@gmail.com>
2021-11-12 17:45:35 -0700

    commit tree d8d19506e8f467738f1d01454dc2c62e2c150124

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(sentence) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+
+    let wordsArray = sentence.split(" ");
+    let results = []
+    
+    wordsArray.map(word => {
+      if (wordsArray[0] === word) {
+        results.push(this.capitalize(word))
+      } else {
+        if (exceptions.includes(word)) {
+          results.push(word)
+        } else {
+          results.push(this.capitalize(word))
+        }
+      }
+    })
+
+    return results.join(' ')
+  }
 }
